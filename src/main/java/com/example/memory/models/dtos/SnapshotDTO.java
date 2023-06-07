caed7a0954967716c993f237a51e893b2015c8df
Nanda Binod <nanda.sreetha@gmail.com>
2023-06-07 20:23:21 +0530

    dynamodb

--- src/main/java/com/example/memory/models/dtos/SnapshotDTO.java
+++ src/main/java/com/example/memory/models/dtos/SnapshotDTO.java
@@ -1,14 +1,16 @@
 package com.example.memory.models.dtos;
 
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 @Value
 @Builder
 public class SnapshotDTO {
     long id;
-    LocalDateTime time;
-    // content
+    Instant time; // convert to OffsetDateTime for storage
+
+
 
 }
